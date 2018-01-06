e66ac0c1f13e106c91bf1d76c52d753ac08c1de1
Bler <lokaen@gmail.com>
2018-01-06 11:05:20 -0500

    Only allow liquizit, hdt, titan on NV
    remmoved additions instead of deleting them later in script

--- mineralconfigs_DATA.sql
+++ mineralconfigs_DATA.sql
@@ -10,21 +10,6 @@ INSERT INTO [dbo].[mineralconfigs]
            ,[totalamountpernode]
            ,[minthreshold])
      VALUES
-           (0
-           ,3
-           ,8
-           ,314
-           ,37500000
-           ,.5)
-
-INSERT INTO [dbo].[mineralconfigs]
-           ([zoneid]
-           ,[materialtype]
-           ,[maxnodes]
-           ,[maxtilespernode]
-           ,[totalamountpernode]
-           ,[minthreshold])
-     VALUES
            (8
            ,3
            ,8
@@ -41,14 +26,11 @@ UPDATE [dbo].[mineralconfigs]
       ,[minthreshold] = .5
  WHERE ID=8
 
-UPDATE [dbo].[mineralconfigs]
-   SET [zoneid] = 0
-      ,[materialtype] = 4
-      ,[maxnodes] = 8
-      ,[maxtilespernode] = 314
-      ,[totalamountpernode] = 37500000
-      ,[minthreshold] = .5
- WHERE ID=7
+ --Only Liquizit, HDT, Titan on NV
+DELETE FROM [dbo].[mineralconfigs]
+	WHERE ID=5
 
+DELETE FROM [dbo].[mineralconfigs]
+	WHERE ID=7
 
 GO
