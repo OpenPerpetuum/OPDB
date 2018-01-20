eaeca829727b6fa7cb749ed3b5f93e9906be7c0c
mikejeffers <mike.s.jeffers@gmail.com>
2018-01-20 00:39:23 -0500

    full pre-alpha content state; no mk2s, t2+t4+, elites, lotterytickets, and pbs items

--- Content_Withholding/REMOVAL_t2_t4_plus_from_npcloot.sql
+++
@@ -1,8 +0,0 @@
---SELECT * FROM [perpetuumsa].[dbo].[entitydefaults]
---JOIN npcloot ON lootdefinition=entitydefaults.definition
---WHERE tiertype = 3 AND (tierlevel=2 or tierlevel=4);
-
---Remove all T2+/T4+ from npc loot 
-DELETE loot FROM [perpetuumsa].[dbo].npcloot as loot
-Join entitydefaults on lootdefinition=entitydefaults.definition
-where tiertype = 3 and (tierlevel=2 or tierlevel=4)
