3449b44c3710b1a9285968be67d1b2a45c137673
Bler <lokaen@gmail.com>
2018-01-25 20:07:54 -0500

    scarab fix fix flock id's NULL!

--- Staging_Dev/prealpha_patch_1.sql
+++ Staging_Dev/prealpha_patch_1.sql
@@ -4123,9 +4123,12 @@ UPDATE [dbo].[npcpresence]
 DECLARE @flockID int;
 DECLARE @definitionID int;
 SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_npc_dummy_decoy' ORDER BY definition DESC);
+SET @flockID = (SELECT TOP 1 id from [npcflock] WHERE [name] = 'training_flock_a' ORDER BY id DESC);
 UPDATE [dbo].[npcflock] SET [name] = 'training_flock_a' ,[presenceid] = @presenceID, [flockmembercount] = 20, [definition] = 1473, [spawnoriginX] = 676, [spawnoriginY] = 414 ,[spawnrangeMin] = 10, [spawnrangeMax] = 20,[respawnseconds] = 0, [totalspawncount] = 0, [homerange] = 30 ,[note] = '', [respawnmultiplierlow] = 1, [enabled] = 1, [iscallforhelp] = 0, [behaviorType] = 1 WHERE id=@flockID;
 SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_npc_scarab_training' ORDER BY definition DESC);
+SET @flockID = (SELECT TOP 1 id from [npcflock] WHERE [name] = 'training_flock_scarab' ORDER BY id DESC);
 UPDATE [dbo].[npcflock] SET [name] = 'training_flock_scarab' ,[presenceid] = @presenceID, [flockmembercount] = 3, [definition] = 5334, [spawnoriginX] = 713, [spawnoriginY] = 406 ,[spawnrangeMin] = 1, [spawnrangeMax] = 2,[respawnseconds] = 0, [totalspawncount] = 0, [homerange] = 2 ,[note] = '', [respawnmultiplierlow] = 1, [enabled] = 1, [iscallforhelp] = 0, [behaviorType] = 1 WHERE id=@flockID;
+
 
 GO
 
