dfe0cb4ca14d2a886de2eed1723399a8d2751221
Unavailable <2634337+MikeJeffers@users.noreply.github.com>
2018-02-24 21:44:57 -0500

    Localization problem on height fields + med aux patch (#45)
    * Bot balance v3, ammo v1, mods v3; hitsize, slots, mass
    
    * incorporated missing changes for metis and kain
    
    * some fixes and adds; all outputs clean and approved
    
    * localization error from tool on #height fields!
    
    * med aux
    
    * cleaned and tested

---
+++ Staging_Dev/BALANCE_med_aux__2018_02_24.sql
@@ -0,0 +1,152 @@
+USE [perpetuumsa]
+GO
+
+PRINT N'Medium Aux accum changes';
+
+DECLARE @definitionID int;
+DECLARE @aggvalueID int;
+DECLARE @aggfieldID int;
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_standard_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_standard_medium_core_battery', quantity=1, attributeflags=589828, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t1', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_standard_core_battery_desc', purchasable=1, tiertype=1, 
+                tierlevel=1 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=105 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named1_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named1_medium_core_battery', quantity=1, attributeflags=589824, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t2', 
+                note='', enabled=1, volume=1, mass=1350, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=1, 
+                tierlevel=2 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=100 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named2_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named2_medium_core_battery', quantity=1, attributeflags=589824, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t3', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=1, 
+                tierlevel=3 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=107 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named3_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named3_medium_core_battery', quantity=1, attributeflags=589828, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t4', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=1, 
+                tierlevel=4 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=109 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named1_medium_core_battery_pr' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named1_medium_core_battery_pr', quantity=1, attributeflags=589824, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t2_pr', 
+                note='', enabled=1, volume=1, mass=1350, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=2, 
+                tierlevel=2 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=97 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named2_medium_core_battery_pr' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named2_medium_core_battery_pr', quantity=1, attributeflags=589824, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t3_pr', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=2, 
+                tierlevel=3 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=103 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named3_medium_core_battery_pr' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_named3_medium_core_battery_pr', quantity=1, attributeflags=589828, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t4_pr', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_named_core_battery_desc', purchasable=1, tiertype=2, 
+                tierlevel=4 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=105 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_artifact_damaged_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_artifact_damaged_medium_core_battery', quantity=1, attributeflags=589828, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t0', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_artifact_damaged_core_battery_desc', purchasable=1, tiertype=1, 
+                tierlevel=0 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=105 WHERE id =  @aggvalueID;
+
+
+
+
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_artifact_a_medium_core_battery' ORDER BY definition DESC);
+
+UPDATE entitydefaults Set definitionname='def_artifact_a_medium_core_battery', quantity=1, attributeflags=589824, categoryflags=34079503, options='#moduleFlag=i20#tier=$tierlevel_t3a', 
+                note='', enabled=1, volume=1, mass=1500, hidden='False', health=100, descriptiontoken='def_artifact_a_core_battery_desc', purchasable=1, tiertype=3, 
+                tierlevel=3 where definition=@definitionID;
+
+
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'powergrid_usage' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=115 WHERE id =  @aggvalueID;
+
+
+
+GO
