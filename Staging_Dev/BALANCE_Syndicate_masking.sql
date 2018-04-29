452575a61a8dfb56b2e031de67a5b60cb0d871b0
mikejeffers <mike.s.jeffers@gmail.com>
2018-04-29 12:51:25 -0400

    remove unapproved changes, adjust others, additions next commit

--- Staging_Dev/BALANCE_Syndicate_masking.sql
+++ Staging_Dev/BALANCE_Syndicate_masking.sql
@@ -2,7 +2,7 @@ USE [perpetuumsa]
 GO
 
 
---------------Syndicate Combat masking buff--------------
+--------------Syndicate Combat masking buff mech/Hmech only--------------
 
 DECLARE @definitionID int;
 DECLARE @aggvalueID int;
@@ -33,29 +33,6 @@ SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @de
 UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=110 WHERE id =  @aggvalueID;
 
 
------------Keep masking same for lights/assaults--------------
-PRINT N'def_locust_head';
-SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_locust_head' ORDER BY definition DESC);
-UPDATE entitydefaults Set definitionname='def_locust_head', quantity=1, attributeflags=1024, categoryflags=131408, options='#height=f0.01#slotFlags=48,8,8,8,8', 
-                note='', enabled=1, volume=1, mass=300, hidden='True', health=100, descriptiontoken='bot_head_desc', purchasable=0, tiertype=0, 
-                tierlevel=0 where definition=@definitionID;
-
-
-SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
-SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=110 WHERE id =  @aggvalueID;
-
-
-
-PRINT N'def_vektor_head';
-SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_vektor_head' ORDER BY definition DESC);
-UPDATE entitydefaults Set definitionname='def_vektor_head', quantity=1, attributeflags=1024, categoryflags=65872, options='#height=f0.01#slotFlags=48,8,8,8', 
-                note='', enabled=1, volume=1, mass=300, hidden='True', health=100, descriptiontoken='bot_head_desc', purchasable=0, tiertype=0, 
-                tierlevel=0 where definition=@definitionID;
-
-
-SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
-SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=105 WHERE id =  @aggvalueID;
+-----------Keep masking same for lights/assaults/ewar--------------
 
 GO
