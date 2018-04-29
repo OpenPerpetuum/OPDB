aad99a11ec0dde29463736f0ce453d50025ed5fb
mikejeffers <mike.s.jeffers@gmail.com>
2018-04-29 13:10:07 -0400

    revert selants and erps to vanilla stats

---
+++ Staging_Dev/BALANCE_reactor_sealants-revertvanilla.sql
@@ -0,0 +1,66 @@
+USE [perpetuumsa]
+GO
+
+
+DECLARE @definitionID int;
+DECLARE @aggvalueID int;
+DECLARE @aggfieldID int;
+
+PRINT N'def_standard_reactor_sealing';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_standard_reactor_sealing' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.75 WHERE id =  @aggvalueID;
+
+
+PRINT N'def_named1_reactor_sealing';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named1_reactor_sealing' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.75 WHERE id =  @aggvalueID;
+
+
+PRINT N'def_named2_reactor_sealing';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named2_reactor_sealing' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.7 WHERE id =  @aggvalueID;
+
+
+PRINT N'def_named3_reactor_sealing';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named3_reactor_sealing' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.65 WHERE id =  @aggvalueID;
+
+
+
+----PROTOTYPES---
+PRINT N'def_named1_reactor_sealing_pr';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named1_reactor_sealing_pr' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.75 WHERE id =  @aggvalueID;
+
+
+PRINT N'def_named2_reactor_sealing_pr';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named2_reactor_sealing_pr' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.7 WHERE id =  @aggvalueID;
+
+
+PRINT N'def_named3_reactor_sealing_pr';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_named3_reactor_sealing_pr' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'reactor_radiation_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=0.65 WHERE id =  @aggvalueID;
+
+GO
