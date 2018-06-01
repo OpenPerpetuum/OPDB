0c08bc18054331831758df466298cf48b594bd57
mikejeffers <mike.s.jeffers@gmail.com>
2018-05-31 21:57:33 -0400

    timer to 8hr, price to 800k

--- Staging_Dev/MARKET_probe_price.sql
+++ Staging_Dev/MARKET_probe_price.sql
@@ -1,14 +1,14 @@
 USE [perpetuumsa]
 GO
 
---SET PROBE SELL PRICE TO 4M
-PRINT N'Set price of def_visibility_probe_capsule to 4mil';
+--SET PROBE SELL PRICE TO 800k
+PRINT N'Set price of def_visibility_probe_capsule to 800k';
 DECLARE @def int;
 DECLARE @sellprice float;
 
 
 SET @def = (SELECT TOP 1 definition FROM entitydefaults where definitionname='def_visibility_probe_capsule');
-SET @sellprice = 4000000;
+SET @sellprice = 800000;
 UPDATE marketitems SET price = @sellprice
 WHERE itemdefinition = @def 
 and isSell=1 and isvendoritem=1;
--- Staging_Dev/def_visibility_probe_capsule__2018_05_20_18_05_43.sql
+++ Staging_Dev/def_visibility_probe_capsule__2018_05_20_18_05_43.sql
@@ -2,7 +2,7 @@ USE [perpetuumsa]
 GO
 
 
---ADD DESPAWN TIME FIELD TO PROXIMITY PROBE
+--ADD DESPAWN TIME FIELD TO PROXIMITY PROBE (8hr)
 PRINT N'ADD DESPAWN TIME FIELD TO PROXIMITY PROBE';
 
 DECLARE @definitionID int;
@@ -12,5 +12,7 @@ SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definiti
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'despawn_time' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-INSERT INTO [dbo].[aggregatevalues] ([definition],[field],[value]) VALUES (@definitionID, @aggfieldID, 10800000);
+INSERT INTO [dbo].[aggregatevalues] ([definition],[field],[value]) VALUES (@definitionID, @aggfieldID, 28800000);
+
+GO
 
