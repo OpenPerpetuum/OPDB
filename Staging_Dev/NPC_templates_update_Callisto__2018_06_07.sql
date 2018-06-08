cdf84eb6dbe0fa839f8489a6f5537b17e0f9450f
mikejeffers <mike.s.jeffers@gmail.com>
2018-06-07 20:38:44 -0400

    template update on nv alpha boss npc

---
+++ Staging_Dev/NPC_templates_update_Callisto__2018_06_07.sql
@@ -0,0 +1,5 @@
+DECLARE @templateID int
+SET @templateID = (SELECT TOP 1 id from robottemplates WHERE [name] = 'Callisto' ORDER BY id DESC)
+
+UPDATE robottemplates SET name='Callisto', description='#robot=i1590#head=i1591#chassis=i1592#leg=i1593#container=i149#headModules=[|m0=[|definition=i2b|slot=i1]|m1=[|definition=i33|slot=i4]]#chassisModules=[|m0=[|definition=i3d|slot=i1|ammoDefinition=i986|ammoQuantity=i0]|m1=[|definition=i3d|slot=i2|ammoDefinition=i986|ammoQuantity=i0]|m2=[|definition=i27|slot=i3]]#legModules=[|m0=[|definition=i10|slot=i1]|m1=[|definition=i19|slot=i2]|m2=[|definition=i12a|slot=i3]|m3=[|definition=i13|slot=i4]]', note='Using for boss bot' WHERE id=@templateID;
+
