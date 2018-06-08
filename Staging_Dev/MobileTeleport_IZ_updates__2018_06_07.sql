9a8f0504eaf8ca4c73df39f9991bc5519ea3eceb
Unavailable <2634337+MikeJeffers@users.noreply.github.com>
2018-06-08 09:58:40 -0400

    Interzone changes + NV scooch (#77)
    * interzone param changes
    
    * move NV to the east to prevent IZ from/to NV

---
+++ Staging_Dev/MobileTeleport_IZ_updates__2018_06_07.sql
@@ -0,0 +1,53 @@
+USE [perpetuumsa]
+GO
+
+--UPDATE IZ TELEPORT AND MOVE ZONE0 EAST
+
+DECLARE @aggvalueID int;
+DECLARE @aggfieldID int;
+DECLARE @definitionID int;
+DECLARE @sellprice float;
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_mobile_world_teleport_capsule' ORDER BY definition DESC);
+
+PRINT N'SET IZ teleport to 1.5m->1.5m';
+
+SET @sellprice = 1500000;
+
+--SET IZ teleport to 1.5m
+UPDATE marketitems SET price = @sellprice
+WHERE itemdefinition = @definitionID 
+and isSell=1 and isvendoritem=1;
+
+
+--SET IZ teleport Volume 60U->8U
+PRINT N'SET IZ teleport Volume 60U->8U';
+UPDATE entitydefaults Set definitionname='def_mobile_world_teleport_capsule', quantity=1, attributeflags=25167872, categoryflags=131480, options='#target=n3434', 
+                note='ez lesz a konténeredben, jobbegér-use es csinal mobile world teleportot', enabled=1, volume=8, mass=50000, hidden='False', health=100, descriptiontoken='def_mobile_world_teleport_capsule_desc', purchasable=1, tiertype=0, 
+                tierlevel=0 where definition=@definitionID;
+
+
+--SET IZ teleport despawn time 900s->600s
+PRINT N'SET IZ teleport despawn time 900s->600s';
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'despawn_time' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=600000 WHERE id =  @aggvalueID;
+
+--SET IZ teleport stealth_strength  25->50
+PRINT N'SET IZ teleport stealth_strength  25->50';
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=50 WHERE id =  @aggvalueID;
+
+GO
+
+
+--MOVE NV FURTHER EAST TO BE OUT OF IZ RANGE W/ 30,720m
+PRINT N'MOVE NV FURTHER EAST (x+) 4000x->5000x';
+UPDATE zones
+SET x = 5000, y = 1000
+WHERE
+id = (SELECT TOP 1 id FROM zones WHERE name='zone_TM' or note='new virginia');
+
+GO
