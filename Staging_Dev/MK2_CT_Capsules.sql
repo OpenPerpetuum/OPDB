1daa217ab2cfe4b7b8c156c07827b12b817d645b
mikejeffers <mike.s.jeffers@gmail.com>
2018-10-15 21:33:34 -0400

    Re-enable all mk2 stuff; script compile and cleanup

--- Staging_Dev/MK2_CT_Capsules.sql
+++ Staging_Dev/MK2_CT_Capsules.sql
@@ -1,6 +1,126 @@
 USE [perpetuumsa]
 GO
 
+-----------------RE-INTRODUCE MK2 CTs and BOTs-------------------
+--Renable all Mk2 CT's and Mk2 robots
+UPDATE entitydefaults SET enabled=1, hidden=0
+WHERE categoryflags=(SELECT TOP 1 value FROM categoryFlags WHERE name='cf_dynamic_cprg');
+
+UPDATE entitydefaults SET enabled=1, hidden=0, purchasable=1
+WHERE definitionname like '%mk2_bot' and tierlevel=2 and tiertype=1;
+-----------------------------------------------------------------
+
+
+--------------------CREATE MK2 CT CAPSULES-----------------------
+--Creates new EntityDefaults and a new Table relating 2 EntityDefaults
+--The "Definition" in [calibrationtemplateitems] is the new CT "capsule"
+--The "TargetDefinition" is the MK2 CT template item itself
+
+USE [perpetuumsa]
+GO
+
+DROP TABLE IF EXISTS #TEMP
+--Create table to build new EntityDefault entries
+CREATE TABLE #TEMP  
+(targetdefinition int, 
+definitionname varchar(100), 
+quantity int,
+attributeflags bigint,
+categoryflags bigint,
+options varchar(max),
+note nvarchar(2048),
+enabled bit,
+volume float,
+mass float,
+hidden bit,
+health float,
+descriptiontoken nvarchar(100),
+purchasable bit,
+tiertype int,
+tierlevel int)
+INSERT INTO #TEMP 
+SELECT definition, definitionname, 1, 2052, 1179, '', '',1, 0.1, 0.1, 0, 100, '', 1, 1, 2 FROM entitydefaults where definition in (SELECT definition from dynamiccalibrationtemplates)
+
+--Set fields as necesary
+UPDATE #TEMP SET
+definitionname = REPLACE(definitionname,'_dynamic_cprg','_CT_capsule'),
+quantity = 1,
+attributeflags=2052,
+categoryflags = 1179,
+options = '',
+note = 'MK2 CT Capsule!',
+enabled= 1,
+volume = 0.1,
+mass = 0.1,
+hidden = 0,
+health = 100,
+descriptiontoken ='calibration_program_desc',
+purchasable = 1,
+tierlevel = CASE 
+WHEN definitionname like '%_mk2_A_%' THEN 1
+WHEN definitionname like '%_mk2_B_%' THEN 2
+WHEN definitionname like '%_mk2_C_%' THEN 3 
+ELSE 0 END;
+
+
+--Insert the mk2 CT "capsule" definitions
+INSERT INTO entitydefaults 
+	([definitionname]
+	,[quantity]
+	,[attributeflags]
+	,[categoryflags]
+	,[options]
+	,[note]
+	,[enabled]
+	,[volume]
+	,[mass]
+	,[hidden]
+	,[health]
+	,[descriptiontoken]
+	,[purchasable]
+	,[tiertype]
+	,[tierlevel])
+SELECT 
+	[definitionname]
+	,[quantity]
+	,[attributeflags]
+	,[categoryflags]
+	,[options]
+	,[note]
+	,[enabled]
+	,[volume]
+	,[mass]
+	,[hidden]
+	,[health]
+	,[descriptiontoken]
+	,[purchasable]
+	,[tiertype]
+	,[tierlevel]
+FROM #TEMP
+
+--Create a new table in perpetuumsa DB to link the new Capsule definition to the target definition (the mk2 CT)
+DROP TABLE IF EXISTS [perpetuumsa].[dbo].[calibrationtemplateitems];
+
+CREATE TABLE [perpetuumsa].[dbo].[calibrationtemplateitems]
+(definition int, targetdefinition int);
+
+--Insert the relations
+INSERT INTO [perpetuumsa].[dbo].[calibrationtemplateitems]
+(definition, targetdefinition)
+SELECT entitydefaults.definition, #TEMP.targetdefinition FROM #TEMP 
+join entitydefaults on #TEMP.definitionname = entitydefaults.definitionname;
+
+
+DROP TABLE IF EXISTS #TEMP
+
+GO
+
+--------------Part 2 -----------------
+--Reinsert all artifacts (from vanilla data backup)
+--Swap all artifact CT loot to CT-Capsules
+USE [perpetuumsa]
+GO
+
 --MK2 artifact re-insertion and transform to CT capsules
 
 --REINSERT RAW MK2 CT LOOTS (this is a direct copy from vanilla tables -- excuse the definition and type ids)
@@ -614,7 +734,7 @@ VALUES
 GO
 
 
---Set all the mk2 CT
+--Set all the mk2 CT loot to mk2 CT-capsules
 UPDATE artifactloot
 SET
 definition = (SELECT TOP 1 calibrationtemplateitems.definition FROM calibrationtemplateitems WHERE calibrationtemplateitems.targetdefinition=artifactloot.definition)
