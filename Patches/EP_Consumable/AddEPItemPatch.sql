e450376770464f1b84b5a9896c60e1e37a56c220
Daniel Smith <dan.smith@zyned.com>
2018-12-29 20:40:43 +0000

    Added all three boost items with appropriate values

--- Patches/EP_Consumable/AddEPItemPatch.sql
+++ Patches/EP_Consumable/AddEPItemPatch.sql
@@ -5,7 +5,12 @@ set xact_abort on
 create table #temp_ins_entitydefaults 
 (
 	[definition] int not null,
-	definitionname varchar(100) not null
+	definitionname varchar(100) not null,
+	credit float not null,
+	tmcoin int not null,
+	icscoin int not null,
+	asicoin int not null,
+	unicoin int not null
 );
 
 insert into entitydefaults
@@ -26,7 +31,15 @@ insert into entitydefaults
 	tiertype,
 	tierlevel
 )
-output inserted.[definition], inserted.definitionname into #temp_ins_entitydefaults
+output
+	inserted.[definition] as [definition],
+	inserted.definitionname as definitionname,
+	75000000.0 as credit,
+	36 as tmcoin,
+	36 as icscoin,
+	36 as asicoin,
+	0 as unicoin
+into #temp_ins_entitydefaults
 values
 (
 	'def_boost_ep_t1', --'def_epboost_tier1', -- definitionname
@@ -35,6 +48,98 @@ values
 	1179, -- category flags : 0x49B copied from paint
 	'#addBoost=n2 #timePeriodHours=i18 #tier=$tierlevel_t1', -- options
 	'Adds 2 to the current EP income multiplier for 24 hours', -- note
+	1, -- enabled
+	1E-06, -- volume
+	1E-06, -- mass
+	0, -- hidden
+	100, -- health
+	'def_redeemable_ep_standard_desc', -- descriptiontoken
+	1, --purchasable
+	1, -- tiertype
+	1 -- tierlevel
+);
+
+insert into entitydefaults
+(
+	definitionname,
+	quantity,
+	attributeflags,
+	categoryflags,
+	options,
+	note,
+	[enabled],
+	volume,
+	mass,
+	[hidden],
+	health,
+	descriptiontoken,
+	purchasable,
+	tiertype,
+	tierlevel
+)
+output
+	inserted.[definition] as [definition],
+	inserted.definitionname as definitionname,
+	115000000.0 as credit,
+	60 as tmcoin,
+	60 as icscoin,
+	60 as asicoin,
+	0 as unicoin
+into #temp_ins_entitydefaults
+values
+(
+	'def_boost_ep_t2', --'def_epboost_tier1', -- definitionname
+	1, -- quantity
+	2052, -- attribute flags : 0x804 copied from paint
+	1179, -- category flags : 0x49B copied from paint
+	'#addBoost=n3 #timePeriodHours=i18 #tier=$tierlevel_t2', -- options
+	'Adds 3 to the current EP income multiplier for 24 hours', -- note
+	1, -- enabled
+	1E-06, -- volume
+	1E-06, -- mass
+	0, -- hidden
+	100, -- health
+	'def_redeemable_ep_standard_desc', -- descriptiontoken
+	1, --purchasable
+	1, -- tiertype
+	1 -- tierlevel
+);
+
+insert into entitydefaults
+(
+	definitionname,
+	quantity,
+	attributeflags,
+	categoryflags,
+	options,
+	note,
+	[enabled],
+	volume,
+	mass,
+	[hidden],
+	health,
+	descriptiontoken,
+	purchasable,
+	tiertype,
+	tierlevel
+)
+output
+	inserted.[definition] as [definition],
+	inserted.definitionname as definitionname,
+	185000000.0 as credit,
+	120 as tmcoin,
+	120 as icscoin,
+	120 as asicoin,
+	0 as unicoin
+into #temp_ins_entitydefaults
+values
+(
+	'def_boost_ep_t3', --'def_epboost_tier1', -- definitionname
+	1, -- quantity
+	2052, -- attribute flags : 0x804 copied from paint
+	1179, -- category flags : 0x49B copied from paint
+	'#addBoost=n5 #timePeriodHours=i18 #tier=$tierlevel_t3', -- options
+	'Adds 5 to the current EP income multiplier for 24 hours', -- note
 	1, -- enabled
 	1E-06, -- volume
 	1E-06, -- mass
@@ -58,16 +163,16 @@ insert into itemshop
 	asicoin,
 	unicoin
 )
-select top 1
+select
 	1, --presetid
 	[definition], --targetdefinition
 	1, -- targetamount
-	350000000.00, -- credit
+	credit, -- credit
 	1, --purchasecount
-	5, -- tmcoin
-	5, -- icscoin
-	5, -- asicoin
-	5 -- unicoin
+	tmcoin, -- tmcoin
+	icscoin, -- icscoin
+	asicoin, -- asicoin
+	unicoin -- unicoin
 from #temp_ins_entitydefaults
 
 insert into aggregatevalues
@@ -84,6 +189,7 @@ from #temp_ins_entitydefaults
 join aggregatefields af on af.[name] = 'despawn_time';
 
 select * from #temp_ins_entitydefaults
+drop table #temp_ins_entitydefaults;
 
 commit transaction
 go
