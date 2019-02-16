4f6d7d8cd59db140d17d6afc05e29629762b9646
Philippe Cloutier <philippe.cloutier.1@gmail.com>
2019-02-16 02:30:13 -0500

    * Add new line to the end of the file * Change the query to be more specific to the teleport to remove

--- Staging_Dev/20190215T061300Z_remove_temp_stronghold_teleport.sql
+++ Staging_Dev/20190215T061300Z_remove_temp_stronghold_teleport.sql
@@ -1,6 +1,9 @@
 USE [perpetuumsa]
 GO
 
-delete from dbo.teleportdescriptions where sourcezone = 16 and targetzone = 0;
+delete from dbo.teleportdescriptions
+where id = 403
+and sourcezone = (select id from dbo.zones where name = 'zone_pvp_arena' and note = 'pvp arena')
+and targetzone = (select id from dbo.zones where name = 'zone_TM' and note = 'new virginia');
 
 GO
