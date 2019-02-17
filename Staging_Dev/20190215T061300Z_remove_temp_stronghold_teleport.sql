b4e665bc3a848f545fa94f136a04b96b0774257b
Philippe Cloutier <philippe.cloutier.1@gmail.com>
2019-02-17 15:32:08 -0500

    Replace hardcoded ID with with the description name

--- Staging_Dev/20190215T061300Z_remove_temp_stronghold_teleport.sql
+++ Staging_Dev/20190215T061300Z_remove_temp_stronghold_teleport.sql
@@ -2,7 +2,7 @@ USE [perpetuumsa]
 GO
 
 delete from dbo.teleportdescriptions
-where id = 403
+where description = 'tp_zone_39_3_to_teleport_column_tm_maddis_Z'
 and sourcezone = (select id from dbo.zones where name = 'zone_pvp_arena' and note = 'pvp arena')
 and targetzone = (select id from dbo.zones where name = 'zone_TM' and note = 'new virginia');
 
