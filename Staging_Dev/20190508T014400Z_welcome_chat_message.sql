077fa428c6266d40045b679129f2e19217e9e98b
mikejeffers <mike.s.jeffers@gmail.com>
2019-05-19 20:13:43 -0400

    char name change, add to corp and alliance for lore/immersion?

--- Staging_Dev/20190508T014400Z_welcome_chat_message.sql
+++ Staging_Dev/20190508T014400Z_welcome_chat_message.sql
@@ -10,7 +10,8 @@ GO
 -- A character are also created to allow the chat 
 -- message to be sent from this character.
 --
--- Date: 2019/05/08 
+-- Date: 2019/05/08
+-- Revision 2019/05/19
 ------------------------------------------------
 
 
@@ -75,7 +76,7 @@ INSERT INTO [dbo].[characters]
 VALUES
 			((SELECT accountID FROM dbo.accounts where email = 'OPP_server_account'), --accountID
 			8702057415139945528, 
-			N'New Player Announcer', --nick
+			N'[OPP] Announcer', --nick
 			NULL, 
 			CAST(N'2019-03-17T01:02:00' AS SmallDateTime), 
 			CAST(N'2019-03-17T01:05:00' AS SmallDateTime), 
@@ -105,8 +106,8 @@ VALUES
 			0, 
 			N'#background=i0#morph00=t0000803F#morph01=t00000000#morph02=t00000000#morph03=t6ABCF43E#morph04=t00000000#morph05=t250681BD#morph06=tDF4F0DBE#morph07=tFED4183F#morph08=t6666A63E#morph09=t8716993E#morph10=tE17A14BE#morph11=tFCA9713E#morph12=t00000000#morph13=tF0A746BE#morph14=t508D97BD#morph15=t0681153E#morph16=t00000000#morph17=t00000000#morph18=tC74BB7BE#morph19=t00000000#morph20=tF4FDD4BD#morph21=t21B0323F#morph22=t0681153E#morph23=t819583BE#morph24=tC74B373E#morph25=tCDCCCC3E#morph26=tD7A3F03E#morph27=t00000000#morph28=t00000000#morph29=t00000000#morph30=t00000000#morph31=t00000000#camerax=tD9CEF73D#cameray=tB6F3BD3E#light1posx=tCDCC4CBE#light1posy=t23DBB93E#light1posz=t2B8716BF#light1color=Lffffffff#light2posx=tEC51583F#light2posy=t5839343D#light2posz=tAC1C3ABF#light2color=Lff393939#eyex=tCDCC0C3F#eyey=t6F12033F#eyecolor=Lff67442e#body=i0#bodycolor=Lff572100#hair=i3#haircolor=Lff66809b#beard=i8#eyebrow=ia#beardcolor=Lff66809b#tattoo=i6#tattoocolor=Lff000000#facecolor=Lffb7befa', 
 			'OPP Welcome Character - For sending chat messages to announce new players', 
-			499, 
-			2401,
+			(SELECT TOP 1 eid FROM dbo.corporations WHERE name='syndicate_police_central'),
+			(SELECT TOP 1 id FROM dbo.alliances WHERE name='syndicate_police_alliance'),
 			0);
 
 GO
