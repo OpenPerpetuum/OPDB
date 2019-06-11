a70c3726f6efd13ad3a78c4b600c5a3b6e6b613a
mikejeffers <mike.s.jeffers@gmail.com>
2019-06-10 21:55:24 -0400

    sap masking decrease, 50 until team says something new

---
+++ Staging_Dev/SAPs_masking_decr__2019_06_10.sql
@@ -0,0 +1,52 @@
+USE [perpetuumsa]
+GO
+
+------------------------------------------------------
+--SAP Masking decrease from 100->50rf
+--
+--Last modified Date: 2019/06/10
+------------------------------------------------------
+
+PRINT N'Set Masking Value';
+DECLARE @maskingValue float;
+
+SET @maskingValue = 50.0;
+
+DECLARE @definitionID int;
+DECLARE @aggvalueID int;
+DECLARE @aggfieldID int;
+
+---Active Hacking
+PRINT N'def_sap_active_hacking';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_sap_active_hacking' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=@maskingValue WHERE id =  @aggvalueID;
+
+---Passive Hacking
+PRINT N'def_sap_passive_hacking';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_sap_passive_hacking' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=@maskingValue WHERE id =  @aggvalueID;
+
+---Destruction
+PRINT N'def_sap_destruction';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_sap_destruction' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=@maskingValue WHERE id =  @aggvalueID;
+
+---Specimen
+PRINT N'def_sap_specimen_processing';
+SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_sap_specimen_processing' ORDER BY definition DESC);
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'stealth_strength' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=@maskingValue WHERE id =  @aggvalueID;
+
+GO
+
