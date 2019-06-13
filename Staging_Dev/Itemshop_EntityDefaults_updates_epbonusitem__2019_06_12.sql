bc517e99e343efb13749213df7d9091e79e99e86
mikejeffers <mike.s.jeffers@gmail.com>
2019-06-12 23:28:30 -0400

    add aggvalue updates and printlns

--- Staging_Dev/Itemshop_EntityDefaults_updates_epbonusitem__2019_06_12.sql
+++ Staging_Dev/Itemshop_EntityDefaults_updates_epbonusitem__2019_06_12.sql
@@ -15,7 +15,7 @@ SET @epboost1 = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionnam
 SET @epboost2 = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_boost_ep_t2');
 SET @epboost3 = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_boost_ep_t3');
 
-
+PRINT N'Update Entitydefault of boost items, change options(the real stuff) string and note';
 UPDATE entitydefaults SET
 	options = '#addBoost=n2 #timePeriodHours=i30 #tier=$tierlevel_t1',
 	note = 'Adds 2 to the current EP income multiplier for 48 hours'
@@ -31,6 +31,21 @@ UPDATE entitydefaults SET
 	note = 'Adds 5 to the current EP income multiplier for 48 hours'
 WHERE definition=@epboost3;
 
+PRINT N'Update AggregateValues for the items despawntime, info window data to help players understand how long it lasts';
+DECLARE @aggvalueID int;
+DECLARE @aggfieldID int;
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'despawn_time' ORDER BY [name] DESC);
+
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @epboost1 AND [field]=@aggfieldID ORDER BY definition DESC);
+UPDATE aggregatevalues SET definition=@epboost1, field=@aggfieldID, value=172800000 WHERE id =  @aggvalueID;
+
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @epboost2 AND [field]=@aggfieldID ORDER BY definition DESC);
+UPDATE aggregatevalues SET definition=@epboost2, field=@aggfieldID, value=172800000 WHERE id =  @aggvalueID;
+
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @epboost3 AND [field]=@aggfieldID ORDER BY definition DESC);
+UPDATE aggregatevalues SET definition=@epboost3, field=@aggfieldID, value=172800000 WHERE id =  @aggvalueID;
+
+PRINT N'Update itemshop nic pricing to T1=40m, T2=57.5m T3=95m';
 UPDATE itemshop SET
 credit = 40000000
 WHERE targetdefinition=@epboost1;
