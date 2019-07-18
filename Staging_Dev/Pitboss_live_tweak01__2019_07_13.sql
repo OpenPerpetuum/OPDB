21419640496500cf7c2a1fd9202632e3078c28f2
mikejeffers <mike.s.jeffers@gmail.com>
2019-07-17 23:26:47 -0400

    ville updates

--- Staging_Dev/Pitboss_live_tweak01__2019_07_13.sql
+++ Staging_Dev/Pitboss_live_tweak01__2019_07_13.sql
@@ -39,6 +39,12 @@ PRINT N'Update aggfields on npc definition';
 --Pitboss definition
 SET @definitionID = (SELECT TOP 1 definition from entitydefaults WHERE [definitionname] = 'def_npc_Hersh_PitBoss' ORDER BY definition DESC);
 
+--NEW accuracymod (lower hit chance on small doods)
+SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'accuracy_modifier' ORDER BY [name] DESC);
+SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
+
+INSERT INTO [dbo].[aggregatevalues] ([definition],[field],[value]) VALUES (@definitionID, @aggfieldID, 1.25);
+
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'armor_max_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
@@ -52,7 +58,7 @@ UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=12
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'armor_repair_cycle_time_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=1.75 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=2.25 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'core_max_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
@@ -72,7 +78,7 @@ UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=2.
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'damage_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=2.0 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=3.0 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'falloff_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
@@ -107,22 +113,22 @@ UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=2.
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'resist_chemical' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=300 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=375 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'resist_explosive' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=300 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=375 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'resist_kinetic' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=300 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=375 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'resist_thermal' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
 
-UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=300 WHERE id =  @aggvalueID;
+UPDATE aggregatevalues SET definition=@definitionID, field=@aggfieldID, value=375 WHERE id =  @aggvalueID;
 
 SET @aggfieldID = (SELECT TOP 1 id from aggregatefields WHERE [name] = 'speed_max_modifier' ORDER BY [name] DESC);
 SET @aggvalueID = (SELECT TOP 1 id from aggregatevalues WHERE [definition] = @definitionID AND [field]=@aggfieldID ORDER BY definition DESC);
