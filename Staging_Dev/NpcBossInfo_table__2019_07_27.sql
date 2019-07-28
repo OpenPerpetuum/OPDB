b49685653420ee24233d691a7521d31ccffd45d0
mikejeffers <mike.s.jeffers@gmail.com>
2019-07-28 00:38:12 -0400

    npc boss info table for custom boss goodness

---
+++ Staging_Dev/NpcBossInfo_table__2019_07_27.sql
@@ -0,0 +1,28 @@
+USE [perpetuumsa]
+GO
+
+------------------------------------------
+-- NPC Boss Info Table
+-- Table create to handle more detailed settings for a BossFlock
+-- 
+-- Date Modified: 2019/07/27
+------------------------------------------
+
+DROP TABLE IF EXISTS dbo.npcbossinfo;
+
+CREATE TABLE dbo.npcbossinfo(
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	flockid INT NOT NULL,
+	respawnNoiseFactor FLOAT NULL,
+	lootSplitFlag BIT NOT NULL,
+	outpostEID BIGINT NULL,
+	customDeathMessage VARCHAR(128) NULL
+);
+
+--Insert default values
+INSERT INTO dbo.npcbossinfo (flockid, respawnNoiseFactor, lootSplitFlag, outpostEID) 
+SELECT npcflock.id, 0.15, 1, NULL, 'You killed a Boss!'
+FROM npcflock
+WHERE npcflock.npcSpecialType=1;
+
+GO
