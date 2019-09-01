39caeed963ca08b81edc25c475668b43cedd469b
mikejeffers <mike.s.jeffers@gmail.com>
2019-09-01 16:43:08 -0400

    add new cols for further customization on SAP boss behaviours

--- Staging_Dev/NpcBossInfo_table__2019_07_27.sql
+++ Staging_Dev/NpcBossInfo_table__2019_07_27.sql
@@ -5,9 +5,8 @@ GO
 -- NPC Boss Info Table
 -- Table create to handle more detailed settings for a BossFlock
 -- 
--- Date Modified: 2019/07/27
+-- Date Modified: 2019/09/01
 ------------------------------------------
-
 DROP TABLE IF EXISTS dbo.npcbossinfo;
 
 CREATE TABLE dbo.npcbossinfo(
@@ -16,12 +15,14 @@ CREATE TABLE dbo.npcbossinfo(
 	respawnNoiseFactor FLOAT NULL,
 	lootSplitFlag BIT NOT NULL,
 	outpostEID BIGINT NULL,
+	stabilityPts INT NULL,
+	overrideRelations BIT NOT NULL,
 	customDeathMessage VARCHAR(128) NULL
 );
 
 --Insert default values
-INSERT INTO dbo.npcbossinfo (flockid, respawnNoiseFactor, lootSplitFlag, outpostEID, customDeathMessage) 
-SELECT npcflock.id, 0.15, 1, NULL, 'You killed a Boss!'
+INSERT INTO dbo.npcbossinfo (flockid, respawnNoiseFactor, lootSplitFlag, outpostEID, stabilityPts, overrideRelations, customDeathMessage) 
+SELECT npcflock.id, 0.15, 1, NULL, NULL, 0, 'You killed a Boss!'
 FROM npcflock
 WHERE npcflock.npcSpecialType=1;
 
