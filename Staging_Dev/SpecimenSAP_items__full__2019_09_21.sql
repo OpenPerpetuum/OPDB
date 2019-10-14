c360d382571cdadf01c17b97a7e777956e433bd1
mikejeffers <mike.s.jeffers@gmail.com>
2019-10-13 20:28:19 -0400

    rm cursor, now that its one item, fix material bits

--- Staging_Dev/SpecimenSAP_items__full__2019_09_21.sql
+++ Staging_Dev/SpecimenSAP_items__full__2019_09_21.sql
@@ -83,24 +83,6 @@ PRINT N'CLEAR SIEGEITEMS TABLE (Speci sap req items)';
 DELETE FROM [dbo].[siegeitems];
 
 PRINT N'SETUP TEMP TABLES';
-DROP TABLE IF EXISTS #NAME_TABLE
-CREATE TABLE #NAME_TABLE
-(
-	itemName varchar(128),
-	ctName varchar(128),
-	capsuleName varchar(128)
-)
-
-DROP TABLE IF EXISTS #MATERIAL_TABLE
-CREATE TABLE #MATERIAL_TABLE
-(
-	itemName varchar(128),
-	mat01 varchar(128),
-	mat02 varchar(128),
-	mat03 varchar(128),
-	mat04 varchar(128)
-);
-
 DROP TABLE IF EXISTS #AMOUNTS_TABLE
 CREATE TABLE #AMOUNTS_TABLE
 (
@@ -108,12 +90,13 @@ CREATE TABLE #AMOUNTS_TABLE
 	matAmount int
 );
 
-INSERT INTO #NAME_TABLE (itemName, ctName, capsuleName) VALUES
-('def_specimen_sap_item', 'def_specimen_sap_item_dynamic_cprg', 'def_specimen_sap_item_CT_capsule');
+DECLARE @itemName varchar(128);
+DECLARE @ctName varchar(128);
+DECLARE @capsuleName varchar(128);
 
-
-INSERT INTO #MATERIAL_TABLE (itemName, mat01, mat02, mat03, mat04) VALUES
-('def_specimen_sap_item', 'def_titan', 'def_crude', 'def_liquizit', 'def_specimen_sap_item_flux');
+SET @itemName = 'def_specimen_sap_item';
+SET @ctName = 'def_specimen_sap_item_dynamic_cprg';
+SET @capsuleName = 'def_specimen_sap_item_CT_capsule';
 
 INSERT INTO #AMOUNTS_TABLE (matName, matAmount) VALUES
 ('def_titan', 50000),
@@ -128,125 +111,95 @@ INSERT INTO #AMOUNTS_TABLE (matName, matAmount) VALUES
 ('def_specimen_sap_item_flux', 4);
 
 
-DECLARE @itemName varchar(128);
-DECLARE @ctName varchar(128);
-DECLARE @capsuleName varchar(128);
-
-DECLARE db_cursor CURSOR FOR 
-SELECT itemName, ctName, capsuleName FROM #NAME_TABLE
-OPEN db_cursor  
-FETCH NEXT FROM db_cursor INTO  @itemName, @ctName, @capsuleName
-
-WHILE @@FETCH_STATUS = 0  
-BEGIN  
-
-	PRINT N'1. MAKE THE ITEM [entitydefaults]';
-	PRINT N'2. MAKE THE CT [entitydefaults]';
-	PRINT N'3. MAKE THE CT-capsule [entitydefaults]';
-	IF NOT EXISTS (SELECT TOP 1 definition from entitydefaults WHERE definitionname=@itemName)
-	BEGIN
-		INSERT INTO [dbo].[entitydefaults]
-			([definitionname],[quantity],[attributeflags],[categoryflags],[options],[note],[enabled],[volume],[mass],[hidden],[health],[descriptiontoken],[purchasable],[tiertype],[tierlevel])
-		VALUES
-			(@itemName,1,2048,@categoryOfItem,'','Specimen Item',1,16,16,0,100,'def_titanium_desc',1,1,1),
-			(@ctName,1,1024,@dynCtCategory,'','Specimen Item CT',1,0.1,0.1,0,100,'calibration_program_desc',0,1,1),
-			(@capsuleName, 1, 2052, 1179, '', 'CT Capsule', 1, 0.1, 0.1, 0, 100, 'calibration_program_desc', 1, 1, 1);
-	END
-	ELSE
-	BEGIN
-		--Maybe we try different volumes on the item
-		UPDATE [dbo].[entitydefaults] SET
-		volume=16,
-		mass=16,
-		categoryflags=@categoryOfItem
-		WHERE definitionname=@itemName;
-	END
-
-
-	DECLARE @itemDef int;
-	DECLARE @ctDef int;
-	DECLARE @ctCapsuleDef int;
-
-	SET @itemDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@itemName);
-	SET @ctDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@ctName);
-	SET @ctCapsuleDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@capsuleName);
-
-	PRINT N'4. Map the CT to produce the ITEM';
-	IF NOT EXISTS (SELECT TOP 1 definition from [calibrationtemplateitems] WHERE definition=@ctCapsuleDef)
-	BEGIN
-		INSERT INTO [dbo].[calibrationtemplateitems]
-			   ([definition],[targetdefinition])
-		 VALUES
-			   (@ctCapsuleDef,@ctDef);
-	END
-
-	PRINT N'5. MAP THE CT TO THE ITEM VIA [dynamiccalibrationtemplates]';
-	IF NOT EXISTS (SELECT TOP 1 definition from [dynamiccalibrationtemplates] WHERE definition=@ctDef)
-	BEGIN
-		INSERT INTO [dbo].[dynamiccalibrationtemplates] ([definition],[materialefficiency],[timeefficiency],[targetdefinition]) 
-		VALUES
-			(@ctDef,180,180,@itemDef);
-	END
-	ELSE
-	BEGIN
-		--maybe we try different efficiencies
-		UPDATE [dbo].[dynamiccalibrationtemplates] SET
-			materialefficiency=180,
-			timeefficiency=180
-		WHERE definition=@ctDef;
-	END
-
-
-	PRINT N'6. INSERT CT INTO SYN SHOP';
-	IF EXISTS (SELECT TOP 1 targetdefinition from [itemshop] WHERE targetdefinition=@ctCapsuleDef)
-	BEGIN
-		PRINT N'FOUND ITEM ALREADY IN SHOP - DELETING'
-		DELETE FROM itemshop
-		WHERE targetdefinition=@ctCapsuleDef;
-		PRINT N'DELETED'
-	END
-
-	INSERT INTO [dbo].[itemshop]
-		([presetid],[targetdefinition],[targetamount],[tmcoin],[icscoin],[asicoin],[credit],[unicoin],[globallimit],[purchasecount],[standing])
+PRINT N'1. MAKE THE ITEM [entitydefaults]';
+PRINT N'2. MAKE THE CT [entitydefaults]';
+PRINT N'3. MAKE THE CT-capsule [entitydefaults]';
+IF NOT EXISTS (SELECT TOP 1 definition from entitydefaults WHERE definitionname=@itemName)
+BEGIN
+	INSERT INTO [dbo].[entitydefaults]
+		([definitionname],[quantity],[attributeflags],[categoryflags],[options],[note],[enabled],[volume],[mass],[hidden],[health],[descriptiontoken],[purchasable],[tiertype],[tierlevel])
 	VALUES
-		(@hershfieldPresetShop,@ctCapsuleDef,1,10,10,10,500000,20,NULL,0,NULL);
-	PRINT N'INSERTED itemshop entry for CT';
-
-	PRINT N'7. INSERT ITEM TO SPECIMEN TABLE';
-	INSERT INTO [dbo].[siegeitems] (definition, minquantity, maxquantity) VALUES
-	(@itemDef, 2, 3);
-
-
-
-	PRINT N'8. INSERT [components] FOR PRODUCTION MATERIAL REQUIREMENTS';
-	IF EXISTS (SELECT TOP 1 definition from [components] WHERE definition=@itemDef)
-	BEGIN
-		PRINT N'Found Existing components for this item... DELETING!!!';
-		DELETE FROM components WHERE definition=@itemDef;
-		PRINT N'Item Production table deleted';
-	END
-
-	PRINT N'Inserting item production table';
-	INSERT INTO [dbo].[components] ([definition],[componentdefinition],[componentamount]) VALUES
-	(@itemDef,
-	(SELECT TOP 1 definition from entitydefaults join #MATERIAL_TABLE on mat01=definitionname WHERE #MATERIAL_TABLE.itemName=@itemName),
-	(SELECT TOP 1 matAmount from #AMOUNTS_TABLE join #MATERIAL_TABLE on mat01=matName)),
-	(@itemDef,
-	(SELECT TOP 1 definition from entitydefaults join #MATERIAL_TABLE on mat02=definitionname WHERE #MATERIAL_TABLE.itemName=@itemName),
-	(SELECT TOP 1 matAmount from #AMOUNTS_TABLE join #MATERIAL_TABLE on mat02=matName)),
-	(@itemDef,
-	(SELECT TOP 1 definition from entitydefaults join #MATERIAL_TABLE on mat03=definitionname WHERE #MATERIAL_TABLE.itemName=@itemName),
-	(SELECT TOP 1 matAmount from #AMOUNTS_TABLE join #MATERIAL_TABLE on mat03=matName)),
-	(@itemDef,
-	(SELECT TOP 1 definition from entitydefaults join #MATERIAL_TABLE on mat04=definitionname WHERE #MATERIAL_TABLE.itemName=@itemName),
-	(SELECT TOP 1 matAmount from #AMOUNTS_TABLE join #MATERIAL_TABLE on mat04=matName));
+		(@itemName,1,2048,@categoryOfItem,'','Specimen Item',1,16,16,0,100,'def_titanium_desc',1,1,1),
+		(@ctName,1,1024,@dynCtCategory,'','Specimen Item CT',1,0.1,0.1,0,100,'calibration_program_desc',0,1,1),
+		(@capsuleName, 1, 2052, 1179, '', 'CT Capsule', 1, 0.1, 0.1, 0, 100, 'calibration_program_desc', 1, 1, 1);
+END
+ELSE
+BEGIN
+	--Maybe we try different volumes on the item
+	UPDATE [dbo].[entitydefaults] SET
+	volume=16,
+	mass=16,
+	categoryflags=@categoryOfItem
+	WHERE definitionname=@itemName;
+END
 
 
-	FETCH NEXT FROM db_cursor INTO  @itemName, @ctName, @capsuleName
-END 
+DECLARE @itemDef int;
+DECLARE @ctDef int;
+DECLARE @ctCapsuleDef int;
 
-CLOSE db_cursor  
-DEALLOCATE db_cursor
+SET @itemDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@itemName);
+SET @ctDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@ctName);
+SET @ctCapsuleDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@capsuleName);
+
+PRINT N'4. Map the CT to produce the ITEM';
+IF NOT EXISTS (SELECT TOP 1 definition from [calibrationtemplateitems] WHERE definition=@ctCapsuleDef)
+BEGIN
+	INSERT INTO [dbo].[calibrationtemplateitems]
+			([definition],[targetdefinition])
+		VALUES
+			(@ctCapsuleDef,@ctDef);
+END
+
+PRINT N'5. MAP THE CT TO THE ITEM VIA [dynamiccalibrationtemplates]';
+IF NOT EXISTS (SELECT TOP 1 definition from [dynamiccalibrationtemplates] WHERE definition=@ctDef)
+BEGIN
+	INSERT INTO [dbo].[dynamiccalibrationtemplates] ([definition],[materialefficiency],[timeefficiency],[targetdefinition]) 
+	VALUES
+		(@ctDef,180,180,@itemDef);
+END
+ELSE
+BEGIN
+	--maybe we try different efficiencies
+	UPDATE [dbo].[dynamiccalibrationtemplates] SET
+		materialefficiency=180,
+		timeefficiency=180
+	WHERE definition=@ctDef;
+END
+
+
+PRINT N'6. INSERT CT INTO SYN SHOP';
+IF EXISTS (SELECT TOP 1 targetdefinition from [itemshop] WHERE targetdefinition=@ctCapsuleDef)
+BEGIN
+	PRINT N'FOUND ITEM ALREADY IN SHOP - DELETING'
+	DELETE FROM itemshop
+	WHERE targetdefinition=@ctCapsuleDef;
+	PRINT N'DELETED'
+END
+
+INSERT INTO [dbo].[itemshop]
+	([presetid],[targetdefinition],[targetamount],[tmcoin],[icscoin],[asicoin],[credit],[unicoin],[globallimit],[purchasecount],[standing])
+VALUES
+	(@hershfieldPresetShop,@ctCapsuleDef,1,10,10,10,500000,20,NULL,0,NULL);
+PRINT N'INSERTED itemshop entry for CT';
+
+PRINT N'7. INSERT ITEM TO SPECIMEN TABLE';
+INSERT INTO [dbo].[siegeitems] (definition, minquantity, maxquantity) VALUES
+(@itemDef, 2, 3);
+
+
+
+PRINT N'8. INSERT [components] FOR PRODUCTION MATERIAL REQUIREMENTS';
+IF EXISTS (SELECT TOP 1 definition from [components] WHERE definition=@itemDef)
+BEGIN
+	PRINT N'Found Existing components for this item... DELETING!!!';
+	DELETE FROM components WHERE definition=@itemDef;
+	PRINT N'Item Production table deleted';
+END
+
+PRINT N'Inserting item production table';
+INSERT INTO [dbo].[components] ([definition],[componentdefinition],[componentamount])
+SELECT @itemDef, (SELECT TOP 1 definition FROM entitydefaults WHERE m.matName=definitionname), m.matAmount FROM #AMOUNTS_TABLE as m
 
 
 
@@ -291,8 +244,6 @@ END
 
 --cleanup
 PRINT N'DROP TEMP TABLES';
-DROP TABLE IF EXISTS #NAME_TABLE
-DROP TABLE IF EXISTS #MATERIAL_TABLE
 DROP TABLE IF EXISTS #AMOUNTS_TABLE
 PRINT N'========SPECIMEN FULL REWORK COMPLETE!============';
 
