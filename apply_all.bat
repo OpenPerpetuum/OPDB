ff0ab023fedc26e12a445d0b3945cf74d6def16d
MikeJeffers <2634337+MikeJeffers@users.noreply.github.com>
2020-03-16 21:13:28 -0400

    Patch 15 (#196)
    * p15 Deployed

--- apply_all.bat
+++ apply_all.bat
@@ -43,6 +43,7 @@ call:applyPatch Live_11 live_patch_11.sql Server
 call:applyPatch Live_12 live_patch_12.sql
 call:applyPatch Live_13 live_patch_13.sql Server
 call:applyPatch Live_14 live_patch_14.sql
+call:applyPatch Live_15 live_patch_15.sql Server
 
 echo.
 echo Setting up the tool admin account..
