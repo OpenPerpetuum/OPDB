122df42daa50154e52c063cbfcde17bafd41717a
MikeJeffers <2634337+MikeJeffers@users.noreply.github.com>
2020-04-19 20:00:43 -0400

    Patch 16 (#203)
    * rollup for p16
    
    * add stronghold teleport item desc
    
    * standing reqs broken on shop, add teleport name
    
    * deployed item needs desc
    
    * beta pboss stat tune up
    
    * make interference more challenging on pboss
    
    * add error code for new tp

--- Patches/Live_16/live_patch_16.sql
+++ Patches/Live_16/live_patch_16.sql
@@ -1,6 +1,265 @@
 USE [perpetuumsa]
 GO
 
+------------------------------------------------------------
+--PATCH 16
+--Including:
+--Anniversarkhe_and_gifts_year2__2020_04_07
+--Entitydefaults__update_ChassisScanners__2020_03_21
+--Entitydefaults_insert_mob_tele_strnghld__2020_04_03
+--NPC_beta1_pbosses__2020_04_07
+--npcs_create_iztable_modify_npcpresence__2020_03_10
+--SP_artifactRefresh__2020_03_19
+--Zoneentity_insert_strnghld_tele_col__2020_04_03
+------------------------------------------------------------
+PRINT N'PATCH 16 BEGIN!';
+------------------------------------------------------------
+PRINT N'SP_artifactRefresh__2020_03_19';
+USE [perpetuumsa]
+GO
+
+IF OBJECT_ID('opp.artifactRefresh', 'P') IS NOT NULL
+	DROP PROCEDURE [opp].[artifactRefresh];
+GO
+
+CREATE PROCEDURE [opp].[artifactRefresh]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @fromDate DATETIME;
+	SET @fromDate= DATEADD(DAY, -7, GETDATE());
+	DELETE a FROM artifacts AS a JOIN artifacttypes AS t ON t.id=a.artifacttype WHERE t.persistent=1 AND a.missionguid IS NULL AND a.created < @fromDate;
+END
+
+GO
+------------------------------------------------------------
+PRINT N'npcs_create_iztable_modify_npcpresence__2020_03_10';
+USE [perpetuumsa]
+GO
+IF NOT EXISTS (
+  SELECT * 
+  FROM   sys.columns 
+  WHERE  object_id = OBJECT_ID(N'[dbo].[npcpresence]') 
+         AND name = 'izgroupid'
+)
+BEGIN
+	ALTER TABLE [dbo].[npcpresence]
+	  ADD izgroupid INT NULL;
+END
+
+DROP TABLE IF EXISTS dbo.npcinterzonegroup;
+
+CREATE TABLE dbo.npcinterzonegroup (
+	id INT NOT NULL IDENTITY PRIMARY KEY,
+	name VARCHAR(64) NOT NULL,
+	respawnTime int NOT NULL DEFAULT 86400,
+	respawnNoiseFactor float NOT NULL DEFAULT 0.15
+);
+GO
+------------------------------------------------------------
+
+PRINT N'Entitydefaults__update_ChassisScanners__2020_03_21';
+USE [perpetuumsa]
+GO
+
+----------------------------------------------------------------
+--Make Chassis Scanners offsensive module (pvp flag to use)
+--Updates entitydefaults.attributeflags
+--Date: 2020/03/21
+----------------------------------------------------------------
+
+PRINT N'UPDATING CHASSIS SCANNER TO BE OFFSENSIVE MODULE';
+DECLARE @offset bigint;
+SET @offset = (SELECT offset FROM attributeFlags WHERE name='offensive_module');
+
+DECLARE @category bigint;
+SET @category = (SELECT TOP 1 value FROM categoryFlags WHERE name='cf_chassis_scanner');
+
+PRINT N'ENTITY DEFS TO BE UPDATED';
+SELECT * FROM entitydefaults WHERE categoryflags=@category;
+
+PRINT N'UPDATE ATTR FLAGS';
+UPDATE entitydefaults SET
+	attributeflags = attributeflags | POWER(2, @offset)
+WHERE categoryflags=@category;
+
+PRINT N'UPDATED ENTITY DEFS';
+SELECT * FROM entitydefaults WHERE categoryflags=@category;
+
+PRINT N'DONE WITH CHASSIS SCANNER FLAG UPDATE';
+GO
+------------------------------------------------------------
+
+------------------------------------------------------------
+
+PRINT N'Entitydefaults_insert_mob_tele_strnghld__2020_04_03';
+USE [perpetuumsa]
+GO
+
+------------------------------------------------------------------
+--Stronghold teleporter item definitions
+--Adds or updates entitydefaults
+--Date modified: 2020/04/03
+------------------------------------------------------------------
+
+
+DECLARE @capsuleName VARCHAR(128);
+DECLARE @fieldDevName VARCHAR(128);
+
+SET @capsuleName = 'def_mobile_teleport_stronghold_capsule';
+SET @fieldDevName = 'def_mobile_teleport_stronghold_basic';
+
+IF NOT EXISTS (SELECT definition FROM entitydefaults WHERE definitionname=@capsuleName)
+BEGIN
+	INSERT INTO [dbo].[entitydefaults] ([definitionname],[quantity],[attributeflags],[categoryflags],[options],[note],[enabled],[volume],[mass],[hidden],[health],[descriptiontoken],[purchasable],[tiertype],[tierlevel]) VALUES
+	(@capsuleName,1,25167872,131480,'','Stronghold teleport capsule',1,4,8000,0,100,'def_mobile_teleport_stronghold_capsule_desc',1,0,0);
+END
+ELSE
+BEGIN
+	UPDATE entitydefaults SET
+		attributeflags = 25167872,
+		categoryflags = 131480,
+		note = 'Stronghold teleport capsule',
+		volume = 4,
+		mass = 8000
+	WHERE definitionname=@capsuleName;
+END
+
+IF NOT EXISTS (SELECT definition FROM entitydefaults WHERE definitionname=@fieldDevName)
+BEGIN
+INSERT INTO [dbo].[entitydefaults] ([definitionname],[quantity],[attributeflags],[categoryflags],[options],[note],[enabled],[volume],[mass],[hidden],[health],[descriptiontoken],[purchasable],[tiertype],[tierlevel]) VALUES
+(@fieldDevName,1,12583936,33948280,'','Stronghold teleport field obj',1,4,8000,0,100,'def_mobile_teleport_stronghold_basic_desc',0,0,0);
+END
+ELSE
+BEGIN
+	UPDATE entitydefaults SET
+		attributeflags = 12583936,
+		categoryflags = 33948280,
+		note = 'Stronghold teleport field ob',
+		volume = 4,
+		mass = 8000
+	WHERE definitionname=@fieldDevName;
+END
+
+
+IF NOT EXISTS (SELECT definition FROM definitionconfig WHERE definition=(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = @capsuleName))
+BEGIN
+	INSERT INTO definitionconfig (definition, targetdefinition, action_delay, note) VALUES
+	((SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = @capsuleName), 
+	(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = @fieldDevName), 
+	120000, 
+	'Stronghold mobile teleport mapping');
+END
+ELSE
+BEGIN
+	UPDATE definitionconfig SET
+		action_delay=120000
+	WHERE definition=(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = @capsuleName);
+END
+
+PRINT N'ADD mobile teleport to itemshop';
+DECLARE @tmShops INT;
+DECLARE @strongholdTeleDef INT;
+SET @strongholdTeleDef = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = @capsuleName);
+
+SET @tmShops = (SELECT TOP 1 id FROM itemshoppresets WHERE name='tm_preset_pve');
+
+IF NOT EXISTS (SELECT [targetdefinition] FROM [itemshop] WHERE [targetdefinition]=@strongholdTeleDef)
+BEGIN
+INSERT INTO [dbo].[itemshop]
+           ([presetid],[targetdefinition],[targetamount],[tmcoin],[icscoin],[asicoin],[credit],[unicoin],[globallimit],[purchasecount],[standing])
+     VALUES
+		   (@tmShops,@strongholdTeleDef,1,100,100,100,1000000,100,NULL,0,NULL);
+END
+ELSE
+BEGIN
+	UPDATE [dbo].[itemshop] SET
+		tmcoin=100,
+		icscoin=100,
+		asicoin=100,
+		credit=1000000,
+		unicoin=100,
+		standing=NULL
+	WHERE presetid=@tmShops AND targetdefinition=@strongholdTeleDef;
+END
+
+GO
+
+
+
+------------------------------------------------------------------
+PRINT N'Zoneentity_insert_strnghld_tele_col__2020_04_03';
+USE [perpetuumsa]
+GO
+
+------------------------------------------------------------------
+--Add teleport column to Omega (stronghold zone)
+--Adds or updates zoneentities
+--Date modified: 2020/04/03
+------------------------------------------------------------------
+
+DECLARE @targetZoneID int;
+SET @targetZoneID = (SELECT TOP 1 id FROM zones WHERE name='zone_pvp_arena' AND zonetype=4);
+
+DECLARE @ename VARCHAR(128);
+SET @ename = 'tp_zone_strnghld_01';
+
+DECLARE @parentEntity bigint;
+SET @parentEntity = (SELECT TOP 1 eid from entitystorage where storage_name='teleport_column');
+
+DECLARE @eid bigint;
+SET @eid = 50000;
+
+IF NOT EXISTS (SELECT eid FROM entities WHERE ename=@ename)
+BEGIN
+	INSERT INTO entities (eid, definition, owner, parent, health, ename, quantity, repackaged, dynprop) VALUES
+	(@eid, 1394, NULL, @parentEntity, 100, @ename, 1, 0, '#enabled=i1');
+END
+ELSE
+BEGIN
+	UPDATE entities SET
+		eid=@eid,
+		definition=1394,
+		parent=@parentEntity,
+		health=100,
+		quantity=1,
+		repackaged=0,
+		dynprop='#enabled=i1'
+	WHERE ename=@ename;
+END
+
+
+IF NOT EXISTS (SELECT eid FROM zoneentities WHERE ename=@ename)
+BEGIN
+	INSERT INTO zoneentities (zoneID, eid, definition, owner, ename, x, y, z, orientation, enabled, note, runtime, synckey) VALUES 
+	(@targetZoneID, @eid, NULL, NULL, @ename, 1108, 1048, 50, 128, 1, 'stronghold teleport 1', 0, 'tp_strnghld_01');
+END
+ELSE
+BEGIN
+	UPDATE zoneentities SET
+		zoneID = @targetZoneID,
+		eid=@eid,
+		x = 1108,
+		y = 1048,
+		z = 50,
+		orientation = 128,
+		enabled = 1,
+		note = 'stronghold teleport 1',
+		runtime = 0,
+		synckey = 'tp_strnghld_01'
+	WHERE ename=@ename;
+END
+
+GO
+
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+PRINT N'NPC_beta1_pbosses__2020_04_07';
+USE [perpetuumsa]
+GO
+
 ----------------------------------------------------------
 --Beta 1 Interzone Pitboss spawns
 --Adds npc, izgroup, and 3 presences of the iz roaming type
@@ -32,14 +291,16 @@ CREATE TABLE #NPCSTATS (
 );
 
 INSERT INTO #NPCSTATS (fieldName, value) VALUES
-('armor_max_modifier', 8.5),
+('armor_max_modifier', 9.5),
 ('armor_repair_amount_modifier', 12.5),
 ('armor_repair_cycle_time_modifier', 0.95),
+('blob_emission_modifier_modifier', 3.0),
+('blob_emission_radius_modifier', 1.5),
 ('core_max_modifier', 15),
-('core_recharge_time_modifier', 0.9),
+('core_recharge_time_modifier', 0.85),
 ('cpu_max_modifier', 2.6),
 ('critical_hit_chance_modifier', 0.2),
-('damage_modifier', 3.1),
+('damage_modifier', 4.1),
 ('detection_strength_modifier',	25),
 ('ecm_strength_modifier', 2.1),
 ('falloff_modifier', 1.6),
@@ -48,14 +309,14 @@ INSERT INTO #NPCSTATS (fieldName, value) VALUES
 ('missile_cycle_time_modifier', 0.85),
 ('optimal_range_modifier', 4.1),
 ('powergrid_max_modifier', 2.1),
-('resist_chemical', 300),
-('resist_explosive', 300),
-('resist_kinetic', 300),
-('resist_thermal', 300),
+('resist_chemical', 900),
+('resist_explosive', 900),
+('resist_kinetic', 900),
+('resist_thermal', 900),
 ('speed_max_modifier', 0.9),
 ('turret_cycle_time_modifier', 0.85),
 ('received_repaired_modifier', 10),
-('sensor_strength_modifier', 15),
+('sensor_strength_modifier', 115),
 ('stealth_strength_modifier', -50);
 
 
@@ -466,4 +727,123 @@ DROP TABLE IF EXISTS #NPCSTATS;
 DROP TABLE IF EXISTS #NPCLOOTS;
 PRINT N'BETA 1 PITBOSS AND IZPRESENCES DEFINED!';
 PRINT N'DONE!';
+GO
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+PRINT N'Anniversarkhe_and_gifts_year2__2020_04_07';
+
+USE [perpetuumsa]
+GO
+
+-----------------------------------------------
+--OPP 2 Year of LIVE server anniversary gifts!
+--New robot definition
+--New Package and gifts
+--Procedure call to distribute gifts
+--Date: 2020/04/07
+-----------------------------------------------
+
+DECLARE @botName VARCHAR(100);
+SET @botName = 'def_anniversarkhe_year2_bot';
+
+
+PRINT N'1. Create entity';
+--WARNING: options string contains references to entitydefaults in hex (Head/leg/chassis are fixed definitions from arkhe mk2)
+--ENTITYDEFAULT SAVE NEW
+INSERT INTO entitydefaults (definitionname, quantity, attributeflags, categoryflags, options, note, enabled,  volume, mass, hidden, health, descriptiontoken, purchasable, tiertype, tierlevel) 
+VALUES (@botName, 1, 0, 257, '#head=iae8#chassis=iae9#leg=iaea#container=i147#tier=$tierlevel_mk2', 'New Arkhe for 2yr aniversary!', 1, 12, 0, 0, 100, CONCAT(@botName, '_desc'), 1, 1, 2); 
+
+
+PRINT N'2. Create template (from tool)';
+--WARNING: description string contains references to entitydefaults in hex
+--TEMPLATE INSERT
+
+DECLARE @definitionHex varchar;
+--This is how to do lookups, convert to hex, and concat into a genxy compatible string!
+SET @definitionHex = (SELECT dbo.ToHex(definition) FROM entitydefaults WHERE definitionname=@botName);
+
+INSERT INTO robottemplates ([name], [description], [note]) 
+VALUES (CONCAT(@botName, '_template'), '#robot=i'+@definitionHex+'#head=iae8#chassis=iae9#leg=iaea#container=i147#headModules=[|m0=[|definition=i0|slot=i1]]#chassisModules=[|m0=[|definition=i0|slot=i1]|m1=[|definition=i0|slot=i2]]#legModules=[|m0=[|definition=i0|slot=i1]|m1=[|definition=i0|slot=i2]]', 'OPP Live 2 year anniversary Reward');
+
+
+PRINT N'3. Create templaterelation';
+INSERT INTO [dbo].[robottemplaterelation]
+           ([definition],[templateid],[itemscoresum],[raceid],[missionlevel],[missionleveloverride],[killep],[note])
+     VALUES
+           ((SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@botName)
+           ,(SELECT TOP 1 id FROM robottemplates WHERE name=CONCAT(@botName, '_template')),0,1,NULL,NULL,NULL,'AnniversArkhe Template Relation year2');
+
+
+PRINT N'4. Insert config w/ desired tint';
+INSERT INTO [dbo].[definitionconfig]
+           ([definition],[tint],[note])
+     VALUES
+           ((SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@botName)
+           ,'#ffaa00'
+           ,'Color for AnniversArkhe2 bot');
+
+
+PRINT N'5. Give bot skills -- ARKHE MK2 HAS NO ENABLER SKILLS RESULT WILL BE 0 rows affected';
+--Use existing bot (that was copied from entitydefault) for extensions
+INSERT INTO [dbo].[enablerextensions] ([definition],[extensionid],[extensionlevel])
+     (SELECT (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@botName), extensionid, extensionlevel FROM enablerextensions WHERE definition=(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_arkhe2_bot'));
+
+PRINT N'Create Packages for pre-alpha participant levels';
+INSERT INTO [dbo].[packages]
+           ([name],[note])
+     VALUES
+           ('opp-anniversary02','2nd year of OPP Live!');
+
+PRINT N'Insert items into Packages for pre-alpha participant levels';
+DECLARE @packid int;
+SET @packid = (SELECT TOP 1 id FROM packages WHERE name='opp-anniversary02');
+
+DECLARE @aid int;
+SET @aid = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_anniversary_package');
+
+DECLARE @specialBot int;
+SET @specialBot = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@botName);
+
+DECLARE @epBoostT2 int;
+SET @epBoostT2 = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_boost_ep_t2');
+
+PRINT N'Items for opp-anniversary02!!!';
+INSERT INTO [dbo].[packageitems]
+           ([packageid],[definition],[quantity])
+     VALUES
+           (@packid, @aid, 2), 
+		   (@packid, @specialBot, 1),
+		   (@packid, @epBoostT2, 1);
+
+GO
+
+
+
+DECLARE @packid int;
+SET @packid = (SELECT TOP 1 id FROM packages WHERE name='opp-anniversary02');
+--TODO use variant of below for future gifting
+--This gift gives to all current accounts at patch-time
+PRINT N'Give everyone a gift! WARNING CURSOR AHEAD!';
+DECLARE @id int;
+DECLARE @pass varchar(100);
+
+DECLARE curse CURSOR FOR SELECT accountID FROM accounts
+OPEN curse
+
+FETCH NEXT FROM curse INTO @id
+
+WHILE @@FETCH_STATUS = 0 BEGIN
+    EXEC dbo.accountPackageProcessOne @id, @packid
+    FETCH NEXT FROM curse INTO @id
+END
+
+CLOSE curse    
+DEALLOCATE curse
+
+GO
+------------------------------------------------------------------
+
+PRINT N'PATCH 16 APPLIED!';
 GO
