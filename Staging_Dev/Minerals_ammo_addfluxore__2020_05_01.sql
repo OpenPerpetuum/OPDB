c4536995bc49e7f5b45ec4b885ef3e928018675d
mikejeffers <mike.s.jeffers@gmail.com>
2020-05-02 00:42:56 -0400

    research tree, production, ct for mining ammo

--- Staging_Dev/Minerals_ammo_addfluxore__2020_05_01.sql
+++ Staging_Dev/Minerals_ammo_addfluxore__2020_05_01.sql
@@ -1,15 +1,19 @@
 USE [perpetuumsa]
 GO
------
+------------------------------------------------------------------
+--Add a new raw material/ore: Flux Ore
 --Insert entitydef geoscandocument
 --Insert entitydef mineral
 --Insert mineral
 --Insert mineralconfigs for zones
 --Insert scanner ammos
 --Insert mining ammo
---Insert production and research for mining ammo TODO
+--Insert production and research for mining ammo
+--
+--
 --geoscanner results show def_<unknown_16>..TODO
-
+--Last modified: 2020/05/01
+------------------------------------------------------------------
 
 
 
@@ -231,4 +235,156 @@ WHEN NOT MATCHED
 	conf.minThreshold);
 DROP TABLE IF EXISTS #MINERALCONFS;
 PRINT N'FLUXORE IS A GO';
+GO
+
+
+----------------------------------------------
+--Set Flux as refined commodity of Flux Ore
+---------------------------------------------
+
+
+PRINT N'Make Flux a proper refined material';
+DECLARE @flux INT;
+SET @flux = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_specimen_sap_item_flux');
+
+DECLARE @fluxOre INT;
+SET @fluxOre = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_fluxore');
+
+PRINT N'SET proper category for Flux';
+UPDATE entitydefaults SET
+	categoryflags = (SELECT TOP 1 value FROM categoryFlags WHERE name='cf_primary_commodities')
+WHERE definition = @flux;
+
+PRINT N'DELETE components for refining flux (0 results if 1st run)';
+SELECT * FROM components WHERE definition  = @flux;
+DELETE FROM components WHERE definition = @flux;
+
+PRINT N'INSERT components for refining flux';
+INSERT INTO components (definition, componentdefinition, componentamount) VALUES
+	(@flux, @fluxOre, 350);
+
+GO
+
+
+----------------------------------------------
+--Add Flux ore mining ammo to indy tree and production pipeline
+---------------------------------------------
+
+DECLARE @fluxAmmo INT;
+DECLARE @epriAmmo INT;
+DECLARE @indyTechTree INT;
+SET @indyTechTree = (SELECT TOP 1 id FROM techtreegroups WHERE name='indy');
+SET @epriAmmo = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_ammo_mining_epriton');
+SET @fluxAmmo = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_ammo_mining_fluxore');
+
+
+IF NOT EXISTS (SELECT TOP 1 [childdefinition] FROM techtree WHERE [childdefinition]=@fluxAmmo)
+BEGIN
+	INSERT INTO techtree ([parentdefinition],[childdefinition],[groupID],[x],[y]) VALUES
+	(@epriAmmo, @fluxAmmo, @indyTechTree, 4, 5);
+END
+ELSE
+BEGIN
+	UPDATE techtree SET
+		parentdefinition=@epriAmmo,
+		groupID=@indyTechTree,
+		x=4,
+		y=5
+	WHERE childdefinition=@fluxAmmo;
+END
+
+select * from techtreepointtypes
+
+--Show and delete existing kernel costs
+SELECT * FROM techtreenodeprices where definition = @fluxAmmo;
+DELETE FROM techtreenodeprices where definition = @fluxAmmo;
+
+--Insert the fresh prices
+INSERT INTO techtreenodeprices (definition, pointtype, amount) VALUES
+(@fluxAmmo, (SELECT TOP 1 id FROM techtreepointtypes WHERE name='hitech'), 6250),
+(@fluxAmmo, (SELECT TOP 1 id FROM techtreepointtypes WHERE name='industrial'), 12500);
+GO
+
+
+--------------------------------------------
+-- Add CT defintion and production data
+---------------------------------------
+
+
+DECLARE @miningAmmoCTCategory BIGINT;
+SET @miningAmmoCTCategory = (SELECT TOP 1 value FROM categoryFlags WHERE name='cf_ammo_mining_calibration_programs');
+
+PRINT N'ADD/UPDATE def_ammo_mining_fluxore ON entitydefaults';
+IF EXISTS (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname='def_ammo_mining_fluxore_cprg')
+BEGIN
+	UPDATE entitydefaults SET
+		definitionname = 'def_ammo_mining_fluxore_cprg',
+		quantity = 1,
+		attributeflags = 1024,
+		categoryflags = @miningAmmoCTCategory,
+		options = NULL,
+		note = NULL,
+		enabled = 1,
+		volume = 0.01,
+		mass = 0.1,
+		hidden = 0,
+		health = 100,
+		descriptiontoken = 'calibration_program_desc',
+		purchasable = 1,
+		tiertype = NULL,
+		tierlevel = NULL
+	WHERE definitionname = 'def_ammo_mining_fluxore_cprg';
+END
+ELSE
+BEGIN
+	INSERT INTO entitydefaults (definitionname, quantity, attributeflags, categoryflags, options, note, enabled, volume, mass, hidden, health, descriptiontoken, purchasable, tiertype, tierlevel) 
+	VALUES 
+	('def_ammo_mining_fluxore_cprg', 1, 1024, @miningAmmoCTCategory, NULL, NULL, 1, 0.01, 1, 0, 100, 'calibration_program_desc', 1, NULL, NULL); 
+END
+
+PRINT N'DELETE calibrationdefaults FOR ANY EXISTING LARGE MOD CTS (0 results if 1st run)';
+SELECT * FROM calibrationdefaults WHERE definition in (SELECT definition FROM entitydefaults WHERE definitionname ='def_ammo_mining_fluxore_cprg');
+DELETE FROM calibrationdefaults WHERE definition in (SELECT definition FROM entitydefaults WHERE definitionname ='def_ammo_mining_fluxore_cprg');
+
+PRINT N'INSERT calibrationdefaults FOR MOD CTS';
+INSERT INTO calibrationdefaults (definition, materialefficiency, timeefficiency)
+SELECT (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_ammo_mining_fluxore_cprg'), 80, 80;
+
+PRINT N'DELETE itemresearchlevels FOR ANY EXISTING LARGE MOD CTS (0 results if 1st run)';
+SELECT * FROM itemresearchlevels WHERE definition in (SELECT definition FROM entitydefaults WHERE definitionname ='def_ammo_mining_fluxore');
+DELETE FROM itemresearchlevels WHERE definition in (SELECT definition FROM entitydefaults WHERE definitionname ='def_ammo_mining_fluxore');
+
+PRINT N'INSERT itemresearchlevels FOR MOD CTS';
+INSERT INTO itemresearchlevels (definition, calibrationprogram, researchlevel, enabled) VALUES
+((SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_ammo_mining_fluxore'), 
+(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_ammo_mining_fluxore_cprg'), 
+1, 1);
+
+
+DROP TABLE IF EXISTS #AMMO_COMPS;
+CREATE TABLE #AMMO_COMPS
+(
+	defName VARCHAR(100),
+	commodityName VARCHAR(100),
+	amount INT
+);
+INSERT INTO #AMMO_COMPS (defName, commodityName, amount) VALUES
+('def_ammo_mining_fluxore', 'def_unimetal', 10),
+('def_ammo_mining_fluxore', 'def_statichnol', 10),
+('def_ammo_mining_fluxore', 'def_isopropentol', 10),
+('def_ammo_mining_fluxore', 'def_metachropin', 10);
+
+
+PRINT N'DELETE components FOR flux mining ammo (0 results if 1st run)';
+SELECT * FROM components WHERE definition IN (SELECT definition FROM entitydefaults WHERE definitionname IN (SELECT DISTINCT defName FROM #AMMO_COMPS));
+DELETE FROM components WHERE definition IN (SELECT definition FROM entitydefaults WHERE definitionname IN (SELECT DISTINCT defName FROM #AMMO_COMPS));
+
+PRINT N'INSERT components FOR flux mining ammo';
+INSERT INTO components (definition, componentdefinition, componentamount)
+SELECT 
+	(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=defName),
+	(SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=commodityName),
+	amount
+FROM #AMMO_COMPS;
+DROP TABLE IF EXISTS #AMMO_COMPS;
 GO
