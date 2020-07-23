5d14a2ab6d952f5024b262f5f572f44a9b6bb6b2
mikejeffers <mike.s.jeffers@gmail.com>
2020-07-22 23:24:57 -0400

    prints

--- Staging_Dev/Modulepropmodifier_delete_paintermask__2020_07_22.sql
+++ Staging_Dev/Modulepropmodifier_delete_paintermask__2020_07_22.sql
@@ -7,12 +7,13 @@ GO
 --Date modified: 2020/07/22
 -----------------------------------------------------------------
 
-
+PRINT N'RECORD TO DELETE';
 SELECT * FROM modulepropertymodifiers WHERE
 categoryflags=(select top 1 value from categoryFlags where name='cf_target_painter') AND
 basefield=(SELECT TOP 1 id FROM aggregatefields WHERE name='effect_stealth_strength_modifier') AND
 modifierfield=(SELECT TOP 1 id FROM aggregatefields WHERE name='effect_enhancer_stealth_strength_modifier');
 
+PRINT N'DELETE 1 modulepropertymodifiers';
 --Delete single modulepropertymodifiers for target painters being modified by signal masker skill bonus field
 DELETE FROM modulepropertymodifiers WHERE
 categoryflags=(select top 1 value from categoryFlags where name='cf_target_painter') AND
