0a6b1cd64d554f4181bcc8c4a34824123665e4ea
docsaintly <docsaintly@gmail.com>
2020-09-19 14:38:20 -0700

    Improved delete function to have more restrictions.

--- Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
+++ Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
@@ -8,4 +8,6 @@ GO
 -- Date: 2020/09/16
 -----------------------------------------------
 
-DELETE FROM zoneentities WHERE id IN (1528, 1529, 1539)
+DELETE FROM zoneentities WHERE id = 1528 and x = 1024.5 and y = 1024.5 and z = 439
+DELETE FROM zoneentities WHERE id = 1529 and x = 1024.5 and y = 1032.5 and z = 439
+DELETE FROM zoneentities WHERE id = 1539 and x = 1113.5 and y = 1049.5 and z = 420
