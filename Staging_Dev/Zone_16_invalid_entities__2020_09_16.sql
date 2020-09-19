93c3ac2523d633309752cb90f89a6f0b447d05ed
docsaintly <docsaintly@gmail.com>
2020-09-19 16:00:15 -0700

    Last commit, hopefully.

--- Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
+++ Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
@@ -8,6 +8,4 @@ GO
 -- Date: 2020/09/16
 -----------------------------------------------
 
-DELETE FROM zoneentities WHERE id = 1528 and x = 1024.5 and y = 1024.5 and z = 439
-DELETE FROM zoneentities WHERE id = 1529 and x = 1024.5 and y = 1032.5 and z = 439
-DELETE FROM zoneentities WHERE id = 1539 and x = 1113.5 and y = 1049.5 and z = 420
+DELETE FROM zoneentities WHERE eid IN (1328, 1329, 1339)
