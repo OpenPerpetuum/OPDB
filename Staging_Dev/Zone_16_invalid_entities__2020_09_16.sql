0c7f7790415de26dd9748292dfd6f65a61e902c2
mikejeffers <mike.s.jeffers@gmail.com>
2020-09-22 19:19:49 -0400

    use 'unique' varchar field

--- Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
+++ Staging_Dev/Zone_16_invalid_entities__2020_09_16.sql
@@ -5,7 +5,8 @@ GO
 -- Some zone entities left over in Zone 16
 -- throwing exceptions during server startup
 --
--- Date: 2020/09/16
+-- Date: 2020/09/22
 -----------------------------------------------
 
-DELETE FROM zoneentities WHERE eid IN (1328, 1329, 1339)
+DELETE FROM zoneentities WHERE eid IN (1328, 1329, 1339) AND synckey IN ('arena_guard', 'tp_back_home', 'tp_back_home2');
+GO
