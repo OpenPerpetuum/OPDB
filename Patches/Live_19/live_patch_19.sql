5039213a108d0c1307774f5f93231a7fd242d8d1
mikejeffers <mike.s.jeffers@gmail.com>
2020-12-13 00:28:34 -0500

    add gift

--- Patches/Live_19/live_patch_19.sql
+++ Patches/Live_19/live_patch_19.sql
@@ -12,6 +12,7 @@ PRINT N'Applying Patch 19';
 -- Npcreinforcements_insert_bossreinforce__2020_11_27
 -- Syndicate_rebalance__newammo__2020_08_30
 -- Zone_16_invalid_entities__2020_09_16
+-- Gifts_xmas__2020_12_13
 ---------------------------------------------------------
 
 
@@ -1174,6 +1175,63 @@ GO
 -----------------------------------------------
 
 DELETE FROM zoneentities WHERE eid IN (1328, 1329, 1339) AND synckey IN ('arena_guard', 'tp_back_home', 'tp_back_home2');
+GO
+
+PRINT N'Gifts_xmas__2020_12_13';
+USE [perpetuumsa]
+GO
+
+PRINT N'DISTRIBUTE GIFTS FOR XMAS PATCH!';
+
+
+DECLARE @packName AS VARCHAR(64) = 'gift-2020-xmas'; --make sure matches in cursor loop
+DECLARE @note AS VARCHAR(MAX) = 'Package for gifting 2020 December Patch';
+
+DECLARE @aidDefName AS VARCHAR(100) = 'def_anniversary_package';
+DECLARE @ikarusDefName AS VARCHAR(100) = 'def_ikarus_bot';
+DECLARE @bombDefName AS VARCHAR(100) = 'def_area_bomb_capsule';
+
+PRINT N'Create Package';
+INSERT INTO [dbo].[packages]
+           ([name],[note])
+     VALUES
+           (@packName,@note);
+
+DECLARE @packid AS INT = (SELECT TOP 1 id FROM packages WHERE name=@packName);
+
+PRINT N'Items for XMAS 2020!!!';
+INSERT INTO [dbo].[packageitems]
+           ([packageid],[definition],[quantity])
+     VALUES
+           (@packid, (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@aidDefName), 2), 
+		   (@packid, (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@ikarusDefName), 1),
+		   (@packid, (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname=@bombDefName), 1);
+GO
+
+
+DECLARE @packid int;
+SET @packid = (SELECT TOP 1 id FROM packages WHERE name='gift-2020-xmas');
+--TODO use variant of below for future gifting
+--This gift gives to all current accounts at patch-time
+PRINT N'Give everyone a gift! WARNING CURSOR AHEAD!';
+DECLARE @id int;
+DECLARE @pass varchar(100);
+
+DECLARE curse CURSOR FOR SELECT accountID FROM accounts
+OPEN curse
+
+FETCH NEXT FROM curse INTO @id
+
+WHILE @@FETCH_STATUS = 0 BEGIN
+    EXEC dbo.accountPackageProcessOne @id, @packid
+    FETCH NEXT FROM curse INTO @id
+END
+
+CLOSE curse    
+DEALLOCATE curse
+
+PRINT N'GIFTS DISTRIBUTED';
+
 GO
 
 PRINT N'Done! Patch 19 Applied';
