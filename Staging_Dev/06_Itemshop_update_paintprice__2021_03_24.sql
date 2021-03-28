75bbd5f37d08c3cc75d0d3eb9f5142b216d4283c
mikejeffers <mike.s.jeffers@gmail.com>
2021-03-28 01:49:55 -0400

    order the patches, zz needs zone stuff first, all paints for nic now

--- Staging_Dev/06_Itemshop_update_paintprice__2021_03_24.sql
+++ Staging_Dev/06_Itemshop_update_paintprice__2021_03_24.sql
@@ -2,12 +2,19 @@ USE [perpetuumsa]
 GO
 -----------------------------------
 -- Reduce Paint price v2
--- Date modified: 2021/03/24
+-- Update price of paints everywhere to 3m nic
+-- no more RGB tokens
+-- Date modified: 2021/03/28
 -----------------------------------
-PRINT N'UPDATE itemshop (15 rows affected)';
+
+PRINT N'UPDATE itemshop (60 rows affected)';
 UPDATE itemshop SET
-	credit=3000000
-WHERE credit=5000000 AND presetid=1 AND
+	credit=3000000,
+	unicoin=NULL,
+	tmcoin=NULL,
+	icscoin=NULL,
+	asicoin=NULL
+WHERE 
 	targetdefinition IN (SELECT definition FROM entitydefaults WHERE
 	categoryflags=(SELECT TOP 1 value FROM categoryFlags WHERE name='cf_paints'));
 
