be7c281da4d849059b2a1d2efa9c49dbbcdd083a
mikejeffers <mike.s.jeffers@gmail.com>
2021-04-10 16:17:21 -0400

    frag volumes

--- Patches/Live_21/live_patch_21.sql
+++ Patches/Live_21/live_patch_21.sql
@@ -33,6 +33,7 @@ GO
 -- ZZ_71_DECOR_IMPORT_zone71_2021_03_31.sql
 -- ZZ_72_DECOR_IMPORT_zone72_2021_03_31.sql
 -- ZZ_73_StrghldExitConfigs_newcol__2021_04_10.sql
+-- ZZ_74_EntityDefaults_fragVol_update__2021_04_10.sql
 --
 -- Date modified: 2021/04/10
 ----------------------------------------------------------------------------
@@ -3832,6 +3833,48 @@ FROM #EXITS;
 
 DROP TABLE IF EXISTS #CONFIGS;
 DROP TABLE IF EXISTS #EXITS;
+GO
+
+
+
+PRINT N'ZZ_74_EntityDefaults_fragVol_update__2021_04_10.sql';
+USE [perpetuumsa]
+GO
+
+---------------------------------------------
+-- Reduce volume of Fragments by 50%
+-- Date Modified: 2021/04/10
+---------------------------------------------
+
+PRINT N'SET FRAGMENT ENTITYDEFAULT VOLUMES';
+DROP TABLE IF EXISTS #FRAGS;
+CREATE TABLE #FRAGS(
+	defName VARCHAR(100),
+	vol FLOAT
+);
+INSERT INTO #FRAGS (defName, vol) VALUES
+('def_robotshard_common_basic',0.005),
+('def_robotshard_common_advanced',0.01),
+('def_robotshard_common_expert',0.015),
+('def_robotshard_thelodica_basic',0.005),
+('def_robotshard_thelodica_advanced',0.01),
+('def_robotshard_thelodica_expert',0.015),
+('def_robotshard_nuimqol_basic',0.005),
+('def_robotshard_nuimqol_advanced',0.01),
+('def_robotshard_nuimqol_expert',0.015),
+('def_robotshard_pelistal_basic',0.005),
+('def_robotshard_pelistal_advanced',0.01),
+('def_robotshard_pelistal_expert',0.015);
+
+PRINT N'MERGE/UPDATE ON FRAGS';
+MERGE [dbo].[entitydefaults] e USING #FRAGS f
+ON e.definitionname=f.defName
+WHEN MATCHED
+    THEN UPDATE SET
+	volume = f.vol;
+
+PRINT N'DONE WITH FRAG VOLUMES';
+DROP TABLE IF EXISTS #FRAGS;
 GO
 
 
