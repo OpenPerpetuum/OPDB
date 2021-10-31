2046bda14cf7ad80d3d2c0dc09366c1e90c92b39
mikejeffers <mike.s.jeffers@gmail.com>
2021-10-31 01:40:02 -0400

    more map tweaks

--- Staging_Dev/01_zones_update_xy__2021_10_30.sql
+++ Staging_Dev/01_zones_update_xy__2021_10_30.sql
@@ -16,7 +16,11 @@ CREATE TABLE #ZONEMAP(
 INSERT INTO #ZONEMAP (zoneId, x, y) VALUES
 ((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_tc_z101'), 774, -36500),
 ((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_tc_z104'), 2147, -15026),
-((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z110'), 1435, -21223);
+((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z108'), -6233, -28442),
+((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z110'), 1435, -21223),
+((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z119'), -18588, -31884),
+((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z126'), 8367, -43372),
+((SELECT TOP 1 id FROM zones WHERE name='zone_gamma_z131'), 17299, -30108);
 
 MERGE [dbo].[zones] AS z
 USING #ZONEMAP AS m
