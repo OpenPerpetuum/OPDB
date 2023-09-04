cf87e1788a352cbe32dbb66441153c44e7908b82
Viktor Dolzhukov <viktor.dolzhukov@intravision.ru>
2023-09-04 20:46:13 +0300

    ﻿Spark Teleport
    - Scripts added for Spark Teleport

---
+++ Patches/Live_30/Raw_SQL/05_def_spark_teleport_hersh_device_INSERT.sql
@@ -0,0 +1,4 @@
+INSERT INTO entitydefaults ( definitionname ,  quantity ,  attributeflags ,  categoryflags ,  options ,  note ,  enabled ,  volume ,  mass ,  hidden , 
+                health ,  descriptiontoken ,  purchasable ,  tiertype ,  tierlevel ) 
+                VALUES ( 'def_spark_teleport_device_hersh', 1, 2052, 328859, '', '', 1, 2, 50000, 0, 100, 'def_spark_teleport_device_hersh_desc', 1, 0, 0); 
+
---
+++ Patches/Live_30/Raw_SQL/07_hershfield_spark_teleport_target_INSERT.sql
@@ -0,0 +1,2 @@
+INSERT INTO [charactersparkteleports] (characterid, baseeid, basedefinition, zoneid, x, y) VALUES
+(0, 142, 1392, 8, 945.5, 788.5)
