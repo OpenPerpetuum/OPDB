e6b059bcb2e85710f3ec34810cec24d30f82f74e
Viktor Dolzhukov <viktor.dolzhukov@intravision.ru>
2023-09-04 21:52:37 +0300

    Spark Teleport
    - General patch script adjusted

--- Patches/Live_30/Raw_SQL/02_respec_token_itemshop_INSERT.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @definition INT
-
-SET @definition = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_respec_token')
-
-INSERT INTO itemshop (presetid, targetdefinition, targetamount, tmcoin, icscoin, asicoin, credit, unicoin, globallimit, purchasecount, standing) VALUES
-(1, @definition, 1, null, null, null, 20000000, 3000, null, 0, null)
