5c686e1e4e234de03387a62e540025844efba589
DESKTOP-8LUE5OF\Admin <crahn.sect@gmail.com>
2023-10-29 15:57:20 +0500

    Fixed error in script that was preventing adding token to syndicate shop

--- Patches/Live_30/Raw_SQL/00_RESPEC_Tokens.sql
+++ Patches/Live_30/Raw_SQL/00_RESPEC_Tokens.sql
@@ -1,14 +1,24 @@
 ---- New category for RESPEC Tokens ----
 
-INSERT INTO categoryflags (value, name, note, hidden, isunique) VALUES
-(263323, 'cf_respec_tokens', 'RESPEC Tokens', 0, 0)
+IF NOT EXISTS (SELECT 1 FROM categoryFlags WHERE name = 'cf_respec_tokens')
+BEGIN
+	INSERT INTO categoryflags (value, name, note, hidden, isunique) VALUES
+	(263323, 'cf_respec_tokens', 'RESPEC Tokens', 0, 0)
+END
 
 GO
 
 ---- RESPEC Token ----
 
-INSERT INTO entitydefaults (definitionname, quantity, attributeflags, categoryflags, options, note, enabled, volume, mass, hidden, health, descriptiontoken, purchasable, tiertype, tierlevel) VALUES
-('def_respec_token', 1, 2052, 263323, '', '', 1, 0.1, 0.1, 0, 100, 'def_respec_token_desc', 1, 0, 0); 
+DECLARE @categoryFlags INT
+
+SET @categoryFlags = (SELECT TOP 1 value FROM categoryFlags WHERE name = 'cf_respec_tokens')
+
+IF NOT EXISTS (SELECT 1 FROM entitydefaults WHERE definitionname = 'def_respec_token')
+BEGIN
+	INSERT INTO entitydefaults (definitionname, quantity, attributeflags, categoryflags, options, note, enabled, volume, mass, hidden, health, descriptiontoken, purchasable, tiertype, tierlevel) VALUES
+	('def_respec_token', 1, 2052, @categoryflags, '#onceInMonths=n1', '', 1, 0.1, 0.1, 0, 100, 'def_respec_token_desc', 1, 0, 0); 
+END
 
 GO
 
@@ -18,7 +28,7 @@ DECLARE @definition INT
 DECLARE @itemshop_preset INT
 
 SET @definition = (SELECT TOP 1 definition FROM entitydefaults WHERE definitionname = 'def_respec_token')
-SET @itemshop_preset = (SELECT TOP 1 definition FROM itemshoppresets WHERE name = 'tm_preset_pve')
+SET @itemshop_preset = (SELECT TOP 1 id FROM itemshoppresets WHERE name = 'tm_preset_pve')
 
 INSERT INTO itemshop (presetid, targetdefinition, targetamount, tmcoin, icscoin, asicoin, credit, unicoin, globallimit, purchasecount, standing) VALUES
 (@itemshop_preset, @definition, 1, null, null, null, 20000000, 3000, null, 0, null)
